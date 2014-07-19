629f67703f1b7f08445cebbf379bc4dfd506f1e7
Santiago Ramirez Ayuso <santiago.ramirez@mulesoft.com>
2014-07-19 02:26:01 -0300

    Add User model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def create
+		@user = User.new(user_params)
+ 
+		@user.save
+		redirect_to users_path
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name)
+		end
+end
