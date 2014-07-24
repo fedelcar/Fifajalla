8e129aa398bed0628c3e597fd01e0e3d0469c8e9
Fede <fedelc.us@gmail.com>
2014-07-23 23:17:33 -0300

    player list fix, added player view page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,11 +1,14 @@
 class PlayersController < ApplicationController
 
 	def index
-		@players = Player.all
+		@players = Player.take(1000)
+		@path = "players/" 
 	end
 
 	def show
-		@player = Player.find_by last_name: params[:id]
+		@player = Player.find_by id: params[:id]
+		@team = Team.find_by id:@player.team_id
+		@user = User.find_by id:@team.user_id
 	end
 
 end
