13c250549413f2bc6cca7b53b184fddc04b82f82
Facu <f_gambarg@hotmail.com>
2014-08-14 04:21:05 -0300

    Propose Simple Trade

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,9 +1,29 @@
 class TradesController < ApplicationController
   def new
-  	@trades=Trade.all
+    current_user = User.find(19)
+    @players_user1 = Player.where("user_id=? or id=?", current_user.id, "62302")
+    @grouped_players1 = @players_user1.inject({}) do |options, player| 
+        (options[Team.find(player.team_id).name] ||= []) << [player.first_name + " " + player.last_name, player.id]
+        options
+    end
+
+    if (params[:id] == "1")
+      @users = User.all
+      @grouped_players2 = @grouped_players1
+    else
+      @users = User.where("id=?", params[:id])
+      @user2 = User.find(params[:id])
+
+      @players_user2 = Player.where("user_id=? or id=?", params[:id], "62302")
+      @grouped_players2 = @players_user2.inject({}) do |options, player| 
+        (options[Team.find(player.team_id).name] ||= []) << [player.first_name + " " + player.last_name, player.id]
+        options
+    end
+    end
+    
+  	@trades = Trade.all
   	@players = Player.all
   	@teams = Team.all
-  	@users = User.all
   end
   
   def onTheBlock
@@ -20,7 +40,6 @@ class TradesController < ApplicationController
   	@players = Player.all
   	@teams = Team.all
   	@users = User.all
-
   end
 
 	def create
@@ -30,9 +49,7 @@ class TradesController < ApplicationController
 		redirect_to trades_path
 	end
 
-
 	private
-	
 		def trade_params
 			params.require(:trade).permit(:player_id, :first_team_id, :first_user_id, :second_team_id, :second_user_id)
 		end
--- db/migrate/20140810025004_add_goal_types.rb
+++ db/migrate/20140810025004_add_goal_types.rb
@@ -1,7 +1,7 @@
 class AddGoalTypes < ActiveRecord::Migration
   def change
     create_table :goal_types do |t|
-    	t.integer :description
+    	t.string :description
     	t.timestamps
     end
   end
